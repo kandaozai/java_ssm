213e7438a5e761bf85fdfc685f86359b07d3342b
Your Real Name <you@email.address>
2019-07-23 08:24:59 +0800

    ssm

--- RebateMall_SSM/src/com/zgx/mapper/IDeadlineDao.java
+++
@@ -1,8 +0,0 @@
-package com.zgx.mapper;
-
-import com.zgx.po.Deadlinetable;
-import com.zgx.common.dao.BaseDao;
-
-public interface IDeadlineDao extends BaseDao<Deadlinetable, Integer>{
-	
-}
--- RebateMall_SSM/src/com/zgx/service/IPlatformBackground.java
+++
@@ -1,8 +0,0 @@
-package com.zgx.service;
-
-import com.zgx.common.service.BaseService;
-import com.zgx.po.Platformbackgroundtable;
-
-public interface IPlatformBackground extends BaseService<Platformbackgroundtable, Integer>{
-	
-}
